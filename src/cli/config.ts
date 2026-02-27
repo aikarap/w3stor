2435da1ca675afaa5b1cdfbfc8dcb1104cb98a56
Alex Karapanos <alexandroskarapanos1@gmail.com>
2026-02-27 14:15:00 +0200

    feat: CLI scaffold with config and HTTP client

---
+++ src/cli/config.ts
@@ -0,0 +1,26 @@
+import Conf from "conf";
+
+const schema = {
+	keystore: {
+		type: "string" as const,
+	},
+	privateKey: {
+		type: "string" as const,
+	},
+	serverUrl: {
+		type: "string" as const,
+		default: "http://localhost:4000",
+	},
+};
+
+const config = new Conf<{
+	privateKey: string;
+	keystore: string;
+	serverUrl: string;
+}>({
+	projectName: "w3stor",
+	projectVersion: "1.0.0",
+	schema,
+});
+
+export default config;
